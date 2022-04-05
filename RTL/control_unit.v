e4556b7de17d4d1bf56a64bb42d5bd830576afa2
Klaas Willems <klaaswillems80@hotmail.com>
2022-04-05 19:51:17 +0200

    code with which we can test branchprediction

--- RTL/control_unit.v
+++ RTL/control_unit.v
@@ -13,14 +13,14 @@ module control_unit(
       output reg        alu_src,
       output reg        reg_write,
       output reg        jump,
-      output reg 	flush,
-      input wire regEqual
+      output reg 	      flush,
+      input wire        regEqual
    );
 
    // RISC-V opcode[6:0] (see RISC-V greensheet)
    parameter integer ALU_R      = 7'b0110011;
    parameter integer ALU_I      = 7'b0010011;
-   parameter integer BRANCH_EQ  = 7'b1100011;
+   parameter integer BRANCH  = 7'b1100011;
    parameter integer JUMP       = 7'b1101111;
    parameter integer LOAD       = 7'b0000011;
    parameter integer STORE      = 7'b0100011;
@@ -30,11 +30,8 @@ module control_unit(
    parameter [1:0] SUB_OPCODE     = 2'b01;
    parameter [1:0] R_TYPE_OPCODE  = 2'b10;
 
-
    //The behavior of the control unit can be found in Chapter 4, Figure 4.18
-
    always@(*)begin
-
       case(opcode)
          ALU_R:begin
             alu_src   = 1'b0;
@@ -45,7 +42,7 @@ module control_unit(
             branch    = 1'b0;
             alu_op    = R_TYPE_OPCODE;
             jump      = 1'b0;
-	    flush     = 1'b0;
+	         flush     = 1'b0;
 	end
 	
 	ALU_I:begin
@@ -57,10 +54,10 @@ module control_unit(
             branch    = 1'b0;
             alu_op    = ADD_OPCODE;
             jump      = 1'b0;	
-	    flush     = 1'b0;
+	         flush     = 1'b0;
 	end
 
-	BRANCH_EQ:begin
+	BRANCH:begin
 		if (regEqual != branchTaken) begin // flush if prediction was wrong
          alu_src   = 1'b0;
          mem_2_reg = 1'b0;
@@ -85,27 +82,27 @@ module control_unit(
 	end
 
 	STORE:begin
-            alu_src   = 1'b1;
-            mem_2_reg = 1'b0;
-            reg_write = 1'b0;
-            mem_read  = 1'b0;
-            mem_write = 1'b1;
-            branch    = 1'b0;
-            alu_op    = ADD_OPCODE;
-            jump      = 1'b0;
-	    flush     = 1'b0;
+         alu_src   = 1'b1;
+         mem_2_reg = 1'b0;
+         reg_write = 1'b0;
+         mem_read  = 1'b0;
+         mem_write = 1'b1;
+         branch    = 1'b0;
+         alu_op    = ADD_OPCODE;
+         jump      = 1'b0;
+         flush     = 1'b0;
 	end
 	
 	LOAD:begin
-            alu_src   = 1'b1;
-            mem_2_reg = 1'b1;
-            reg_write = 1'b1;
-            mem_read  = 1'b1;
-            mem_write = 1'b0;
-            branch    = 1'b0;
-            alu_op    = ADD_OPCODE;
-            jump      = 1'b0;
-	    flush     = 1'b0;
+         alu_src   = 1'b1;
+         mem_2_reg = 1'b1;
+         reg_write = 1'b1;
+         mem_read  = 1'b1;
+         mem_write = 1'b0;
+         branch    = 1'b0;
+         alu_op    = ADD_OPCODE;
+         jump      = 1'b0;
+         flush     = 1'b0;
 	end
 	
 	JUMP:begin
@@ -120,18 +117,19 @@ module control_unit(
 			flush     = 1'b1; // Always flush with jump instruction
 	end
          
-         // Declare the control signals for each one of the instructions here...
-         default:begin
-            alu_src   = 1'b0;
-            mem_2_reg = 1'b0;
-            reg_write = 1'b0;
-            mem_read  = 1'b0;
-            mem_write = 1'b0;
-            branch    = 1'b0;
-            alu_op    = R_TYPE_OPCODE;
-            jump      = 1'b0;
-            flush     = 1'b0;
-         end
+   // Declare the control signals for each one of the instructions here...
+   default:begin
+         alu_src   = 1'b0;
+         mem_2_reg = 1'b0;
+         reg_write = 1'b0;
+         mem_read  = 1'b0;
+         mem_write = 1'b0;
+         branch    = 1'b0;
+         alu_op    = R_TYPE_OPCODE;
+         jump      = 1'b0;
+         flush     = 1'b0;
+   end
+
       endcase
    end
 
