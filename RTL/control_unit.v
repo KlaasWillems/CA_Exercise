9ed0d2819dc65166f8c00d950b6efcccd06c1fc6
Klaas Willems <klaaswillems1@gmail.com>
2022-04-06 20:31:18 +0200

    working jump instruction

--- RTL/control_unit.v
+++ RTL/control_unit.v
@@ -112,7 +112,7 @@ module control_unit(
 	JUMP:begin
 			alu_src   = 1'b0;
 			mem_2_reg = 1'b0;
-			reg_write = 1'b0;
+			reg_write = 1'b1;
 			mem_read  = 1'b0;
 			mem_write = 1'b0;
 			branch    = 1'b0;
